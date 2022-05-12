2ade22fbfda67d413786d80d0d1892c6c5d6b4bc
Christiansly <abiode23@gmail.com>
2022-05-12 19:23:27 +0100

    Banner desgin

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Header from "../components/Header"
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +10,27 @@ const Home: NextPage = () => {
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-     <Header />
+      <Header />
+      <div className="flex items-center justify-between border-y border-black bg-yellow-400 py-10 lg:py-0">
+        <div className="space-y-5 px-10">
+          <h1 className="max-w-xl font-serif text-6xl">
+            {' '}
+            <span className="underline decoration-black decoration-4">
+              Medium
+            </span>{' '}
+            is a place to write, read, and connect
+          </h1>
+          <h2>
+            It's easy and free to post your thinking on any logic and connect
+            with millions of readers.
+          </h2>
+        </div>
+        <img
+          className="hidden h-32 md:inline-flex lg:h-full"
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+          alt=""
+        />
+      </div>
     </div>
   )
 }
